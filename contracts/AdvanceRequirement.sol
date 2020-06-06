fee58c17a90590f8d31688527e4dd704a3b77ea2
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-06-06 08:14:14 -0400

    Update AdvanceRequirement.sol

--- contracts/AdvanceRequirement.sol
+++ contracts/AdvanceRequirement.sol
@@ -1,23 +1,24 @@
 pragma solidity ^0.5.17;
 
 //work in progress, based off of lexDAO's Shame.sol -- use at own risk.
-//simple boolean Advance Requirement confirmation as condition precedent to GATS deal closing
+//simple boolean Advance Requirement confirmation / condition precedent to deal closing
 
-contract  {
+contract Requirement {
     bool private requirementSatisfied = false;
+    string condition;
     
     //set out Advance Requirement details
-    function context() public pure returns(string memory) {
-        return "[Advance Requirement explanation]";
+    function context() public view returns(string memory) {
+        return "Section 4 of the Loan Agreement";
     }
 
     function isrequirementSatisfied() public view returns(bool) {
-        return requirementSatisfied();
+        return requirementSatisfied;
     }
     
     //Favored Party must confirm the Requirement is satisfied
     function confirmSubmit() public {
-        require(msg.sender == [favored party address], "Caller not favored party");
+        require(msg.sender == 0xb7f49E02552751b249caE86959fD50D887708B1D, "Caller not favored party");
         require(requirementSatisfied == false, "Advance Requirement already satisfied");
         requirementSatisfied = true;
     }
