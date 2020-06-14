f468279c3c0325980ba231bb7dc21238d7feb683
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-06-14 09:31:27 -0400

    Create Inbox.sol

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.0;
+
+contract Inbox {
+
+    string public message;
+    
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+    
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+}
