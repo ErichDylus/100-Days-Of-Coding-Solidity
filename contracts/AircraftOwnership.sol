635f6ec826709dc0f8750d3423308cfe260af2c2
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-06-18 12:19:08 -0400

    Create AircraftOwnership.sol
    to implement an NFT for aircraft ownership by string manufacturer, string model, uint MSN, string registration number, bool security interest exists, bool IR registration/interest exists, bool fractional owner

---
+++ contracts/AircraftOwnership.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.6.0;
+
+import "./zombieattack.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+
+/* WORK IN PROGRESS -- USE AT OWN RISK
+ * @dev compliant with OpenZeppelin's implementation of ERC721 */
+
+contract AircraftOwnership is [ZombieAttack], ERC721 {
+
+  using SafeMath for uint256;
+
+  mapping (uint => address) aircraftApprovals;
+
+  function balanceOf(address _owner) external view returns (uint256) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    return zombieToOwner[_tokenId];
+  }
+
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    _transfer(_from, _to, _tokenId);
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+    zombieApprovals[_tokenId] = _approved;
+    emit Approval(msg.sender, _approved, _tokenId);
+  }
+
+}
