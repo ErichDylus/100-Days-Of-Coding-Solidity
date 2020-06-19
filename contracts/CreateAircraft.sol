d60cbacd7b977b710b8cb52e76b71487a70e4d40
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-06-19 15:52:14 -0400

    Update CreateAircraft.sol

--- contracts/CreateAircraft.sol
+++ contracts/CreateAircraft.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
@@ -8,17 +10,17 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract CreateAircraft is Ownable {
 
-  using SafeMath32 for uint32;
+  using SafeMath for uint;
   
-  event NewAircraft(uint aircraftId, string model, string nNumber, uint msn, uint regId);
+  event NewAircraft(string model, string nNumber, uint msn, uint regId, bool faaLienExists, bool capeTownInterest, bool fractionalOwner);
 
   struct Aircraft {
     string model;
     string nNumber;
-    uint32 msn;
+    uint msn;
     uint regId;
     bool faaLienExists;
-    bool capeTownInterest, 
+    bool capeTownInterest; 
     bool fractionalOwner;
   }
 
@@ -27,11 +29,15 @@ contract CreateAircraft is Ownable {
   mapping (uint => address) public aircraftToOwner;
   mapping (address => uint) ownerAircraftCount;
 
-  function _createAircraft(string memory _model, string memory _nNumber, uint32 _msn, uint _regId) internal {
-    uint id = aircraft.push(Aircraft(_model, _nNumber,  _msn, _regId) - 1;
-    aircraftToOwner[id] = msg.sender;
+  function _createAircraft(string memory _model, string memory _nNumber, uint _msn, uint _regId, bool _faaLienExists, bool _capeTownInterest, bool _fractionalOwner) internal {
+    aircraft.push(Aircraft(_model, _nNumber,  _msn, _regId,  _faaLienExists, _capeTownInterest, _fractionalOwner));
+    aircraftToOwner[_regId] = msg.sender;
     ownerAircraftCount[msg.sender] = ownerAircraftCount[msg.sender].add(1);
-    emit NewAircraft(id, _model, _nNumber, _msn, _regId);
+    emit NewAircraft(_model, _nNumber, _msn, _regId, _faaLienExists, _capeTownInterest, _fractionalOwner);
   }
-
+  
+  //****THIS NEEDS WORK
+  function aircraftDetails() public view returns(string memory, string memory, uint, uint, bool, bool, bool) {
+        return (aircraft.model, aircraft.nNumber, aircraft.msn, aircraft.regId, aircraft.faaLienExists, aircraft.capeTownInterest, aircraft.fractionalOwner);
+  }
 }
