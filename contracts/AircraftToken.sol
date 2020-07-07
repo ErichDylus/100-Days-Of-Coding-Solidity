e9a359328cccbc46ad157cae03cad17788846106
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-07-07 19:50:00 -0400

    Update AircraftToken.sol

--- contracts/AircraftToken.sol
+++ contracts/AircraftToken.sol
@@ -6,7 +6,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 // **********WORK IN PROGRESS -- USE AT OWN RISK**********
-// @dev: create an ERC721 NFT for an aircraft, for purposes of the FAA Registry
+// @dev: create an ERC721 NFT for an N-registered aircraft, for purposes of the FAA Registry
 
 contract AircraftToken is ERC721, Ownable {
 
@@ -18,8 +18,7 @@ contract AircraftToken is ERC721, Ownable {
       string nNumber, 
       uint regId, 
       uint msn, 
-      bool faaLienExists, 
-      bool capeTownInterest, 
+      bool lienExists, 
       bool fractionalOwner
       );
 
@@ -29,16 +28,15 @@ contract AircraftToken is ERC721, Ownable {
     string nNumber;
     uint regId;
     uint msn;
-    bool faaLienExists;
-    bool capeTownInterest; 
+    bool lienExists; 
     bool fractionalOwner;
   }
 
   Aircraft[] public aircraft;
   uint i = 0;
   
-  // @dev Initializing an ERC-721 standard token named 'AircraftToken' with a symbol 'AIR'
-  constructor() ERC721("AircraftToken", "AIR") public {
+  // @dev Initializing an ERC-721 standard token named 'AircraftToken' with a symbol 'FAA'
+  constructor() ERC721("FAAToken", "FAA") public {
   }
 
   // fallback function
@@ -51,10 +49,10 @@ contract AircraftToken is ERC721, Ownable {
         string memory _nNumber, 
         uint _regId, 
         uint _msn, 
-        bool _faaLienExists, 
-        bool _capeTownInterest, 
+        bool _lienExists, 
         bool _fractionalOwner
         ) internal returns (uint, uint) {
+    
     // may change to onlyOwner, if intended for registry creation of NFT
     
     Aircraft memory newAircraft = Aircraft({
@@ -63,14 +61,13 @@ contract AircraftToken is ERC721, Ownable {
         nNumber: _nNumber,
         regId: _regId,
         msn: _msn,
-        faaLienExists: _faaLienExists,
-        capeTownInterest: _capeTownInterest,
+        lienExists: _lienExists,
         fractionalOwner: _fractionalOwner
     });
     
-    // create unique aircraft identifier based on owner reg ID, i number and msn, but may not be necessary for tokenID
+    // @dev create unique aircraft identifier based on owner reg ID, i number and msn
     uint newAircraftId = uint(keccak256(abi.encodePacked(_regId + i + _msn)));
-    aircraft.push(Aircraft(_aircraftOwner, _model, _nNumber,  _regId, _msn, _faaLienExists, _capeTownInterest, _fractionalOwner));
+    aircraft.push(Aircraft(_aircraftOwner, _model, _nNumber,  _regId, _msn, _lienExists, _fractionalOwner));
     i++;
     super._mint(_aircraftOwner, newAircraftId);
     emit CreateAircraft(
@@ -79,23 +76,21 @@ contract AircraftToken is ERC721, Ownable {
         newAircraft.nNumber, 
         newAircraft.regId, 
         newAircraft.msn, 
-        newAircraft.faaLienExists, 
-        newAircraft.capeTownInterest, 
+        newAircraft.lienExists, 
         newAircraft.fractionalOwner
         );
     return(newAircraftId, i);
   }
   
-  // return aircraft details based on i number (must be inputted by searcher, for now)
-  function aircraftDetails(uint _i) public view returns(address, string memory, string memory, uint, bool, bool, bool) {
+  // return aircraft details based on i number (must be inputted by searcher and remains viewable after corresponding token burned, for now)
+  function aircraftDetails(uint _i) public view returns(address, string memory, string memory, uint, bool, bool) {
     Aircraft storage regToken = aircraft[_i];
     return (
         regToken.aircraftOwner, 
         regToken.model, 
         regToken.nNumber, 
         regToken.msn, 
-        regToken.faaLienExists, 
-        regToken.capeTownInterest, 
+        regToken.lienExists, 
         regToken.fractionalOwner
         );
   }
@@ -106,12 +101,16 @@ contract AircraftToken is ERC721, Ownable {
         string calldata _nNumber, 
         uint _regId, 
         uint _msn, 
-        bool _faaLienExists, 
-        bool _capeTownInterest, 
+        bool _lienExists, 
         bool _fractionalOwner
         ) external payable returns(uint) {
     require(msg.value >= 0.02 ether);
-    _createAircraft(msg.sender, _model, _nNumber, _regId, _msn, _faaLienExists, _capeTownInterest, _fractionalOwner);
+    _createAircraft(msg.sender, _model, _nNumber, _regId, _msn, _lienExists, _fractionalOwner);
+    }
+    
+    // @dev allow onlyOwner (presumably the Registry) to burn a registry token, for example if the aircraft is deregistered
+    function burnToken(uint _tokenId) public onlyOwner {
+        _burn(_tokenId);
     }
     
     //SEE: https://medium.com/openberry/erc721-vue-js-cryptokitties-like-dapp-in-under-10-minutes-5115efc9e0bb
