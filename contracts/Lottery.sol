0290d10cfd635459d9a0a64a18f98b5692d8530d
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-07-10 15:17:09 -0400

    Update Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -18,7 +18,18 @@ contract Lottery {
       return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
   }
   
-  function pickWinner() public {
+  function pickWinner() public onlyManager {
       uint index = random() % players.length;
+      payable(players[index]).transfer(address(this).balance);
+      players = new address[](0);
   }
+  
+  modifier onlyManager() {
+      require(msg.sender == manager);
+      _;
+  }
+  
+  function getPlayers() public view returns (address[] memory){
+      return(players);
+  } 
 }
