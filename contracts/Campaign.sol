69ee631138cab898d3fabee0ddba2be413e0ceb9
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-07-21 11:39:14 -0400

    Update Campaign.sol

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,19 +1,25 @@
 pragma solidity ^0.6.0;
 
-// in process, not recommended to be used for any purpose
+// in process, for demonstration only, not recommended to be used for any purpose
 
 contract Campaign {
 
   struct Request {
-      string description;
+      string storage description;
       uint value;
       address recipient;
       bool complete;
   }
   
+  Request[] public requests;
   address public manager;
   uint minimumContribution;
   address[] public approvers;
+  
+  modifier restricted() {
+    require(msg.sender == manager);
+    _;
+  }
   
   constructor(uint minimum) public {
       manager = msg.sender;
@@ -23,6 +29,16 @@ contract Campaign {
   function contribute() public payable {
       require(msg.value > minimumContribution);
       approvers.push(msg.sender);
+  }
+  
+  function createRequest(string memory description, uint value, address recipient) public restricted {
+      Request newRequest = Request({
+         description: description,
+         value: value,
+         recipient: recipient,
+         complete: false
+      });
+      requests.push(newRequest);
   }
   
 }
