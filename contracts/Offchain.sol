2819931d57cb67ed8f83ed4848853034835a27cf
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-08-25 11:25:46 -0400

    Update Offchain.sol

--- contracts/Offchain.sol
+++ contracts/Offchain.sol
@@ -3,8 +3,16 @@ pragma solidity ^0.6.0;
 //uses Chainlink's ETH price feed, on Ropsten
 //adapted from https://docs.chain.link/docs/get-the-latest-price 
 
-import "https://github.com/smartcontractkit/chainlink/blob/develop/evm-contracts/src/v0.6/interfaces/AggregatorInterface.sol";
+interface AggregatorInterface {
+  function latestAnswer() external view returns (int256);
+  function latestTimestamp() external view returns (uint256);
+  function latestRound() external view returns (uint256);
+  function getAnswer(uint256 roundId) external view returns (int256);
+  function getTimestamp(uint256 roundId) external view returns (uint256);
 
+  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt);
+  event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
+}
 contract USDConvert {
 
     AggregatorInterface internal priceFeed;
