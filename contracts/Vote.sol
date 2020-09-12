f5ec25a117bfa39f9688b3ac025a05ba02b81f63
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-09-12 19:26:42 -0400

    Update Vote.sol

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -6,23 +6,31 @@ contract Vote {
   // get count of votes for each candidate
   
   bytes32[2] public candidateList;
+  string candidateOneName;
+  string candidateTwoName;
   mapping (bytes32 => uint8) public votesReceived;
 
-  constructor(bytes32 _candidateOne, bytes32 _candidateTwo) public {
-    candidateList = [_candidateOne,_candidateTwo];
-    //candidateList = _candidateNames;
+  constructor(string memory _candidateOne, string memory _candidateTwo) public {
+    bytes32 candidateOne = keccak256(abi.encodePacked(_candidateOne));
+    bytes32 candidateTwo = keccak256(abi.encodePacked(_candidateTwo));
+    candidateList = [candidateOne,candidateTwo];
+    candidateOneName = _candidateOne;
+    candidateTwoName = _candidateTwo;
   }
   
+  //vote for candidate, currently unlimited number of votes
   function voteForCandidate(bytes32 candidate) public {
     require(validCandidate(candidate));
     votesReceived[candidate] += 1;
   }
   
+  //return total votes for candidate
   function totalVotesFor(bytes32 candidate) view public returns(uint8) {
     require(validCandidate(candidate));
     return votesReceived[candidate];
   }
   
+  //check if candidate is valid
   function validCandidate(bytes32 candidate) view public returns (bool) {
     for(uint i = 0; i < candidateList.length; i++) {
       if (candidateList[i] == candidate) {
@@ -30,6 +38,11 @@ contract Vote {
       }
     }
     return false;
+  }
+  
+  //return names of candidates
+  function checkNames() view public returns(string memory, string memory){
+      return (candidateOneName, candidateTwoName);
   }
   
 }
