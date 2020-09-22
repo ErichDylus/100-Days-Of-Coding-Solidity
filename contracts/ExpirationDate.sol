bc9e0613445bc35a8d43601ef6bcf3b439690a62
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-09-22 11:42:10 -0400

    Create ExpirationDate.sol

---
+++ contracts/ExpirationDate.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.6.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
+
+// set expired to true after inputted number of days until expiration
+
+contract Expiration {
+    
+    using SafeMath for uint256;
+    using SafeMath for uint32;
+    
+    uint256 effectiveTime;
+    uint256 expirationTime;
+    uint32 constant DAY_IN_SECONDS = 86400;
+    bool isExpired;
+    
+    constructor() public {
+        effectiveTime = now;
+        isExpired = false;
+    }
+    
+    function inputTerm(uint8 _daysUntilExpiration) public returns(uint256){
+        expirationTime = effectiveTime + uint256(DAY_IN_SECONDS * uint32(_daysUntilExpiration));
+        return(expirationTime);
+    }
+    
+    function expirationDate() public view returns(uint256){
+        return(expirationTime);
+    }
+    
+    function checkIfExpired() public returns(bool){
+        if (expirationTime <= now) {
+            isExpired = true;
+        } else {
+            isExpired = false;
+        }
+        return(isExpired);
+    }
+}
