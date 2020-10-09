453b803b536c7d70149b312d8f9af4b7ce48546e
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-10-09 11:39:25 -0400

    Update CalldataTest.sol

--- contracts/CalldataTest.sol
+++ contracts/CalldataTest.sol
@@ -1,17 +1,24 @@
-pragma solidity 0.5.11;
+pragma solidity 0.6.9;
 
 contract Test {
 
     string stringTest;
-
+    string callTest;
+    
     function memoryTest(string memory _exampleString) public returns (string memory) {
         stringTest = _exampleString;  // You can modify memory
         string memory newString = stringTest;  // You can use memory within a function's logic
         return newString;  // You can return memory
     }
 
-    function calldataTest(string calldata _exampleString) external returns (string memory) {
+    function calldataTest(string calldata _exampleString, string calldata _intTest) external returns (string memory, string memory) {
         stringTest = _exampleString;
-        return stringTest;
+        internalTest(_intTest);
+        return (stringTest, callTest); //return calldata input from this function and once translated to memory in internalTest()
+    }
+    
+    //testing if calldata can be sent to internal functions, successful
+    function internalTest(string calldata _internalCall) internal returns (string memory) {
+        callTest = _internalCall;
     }
 }
