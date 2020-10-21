98e0f3ae10aff030f043236e34fe9f2dbc136536
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-10-21 11:22:28 -0400

    Update CalldataTest.sol

--- contracts/CalldataTest.sol
+++ contracts/CalldataTest.sol
@@ -6,6 +6,7 @@ contract CallandMapTest {
 
     string stringTest;
     string callTest;
+    uint256 ledgerBalance;
     address recipient = 0xb7f49E02552751b249caE86959fD50D887708B1D;
     mapping (address => mapping (address => uint256))  public  allowance;
     mapping(address => uint256) public balances;
@@ -13,19 +14,19 @@ contract CallandMapTest {
    function updateBalance(uint256 newBalance) public {
       balances[msg.sender] = newBalance;
    }
-   
+  /* 
    function updateBalance() public returns (uint256) {
       LedgerBalance ledgerBalance = new LedgerBalance();
       ledgerBalance.updateBalance(10);
       return ledgerBalance.balances(address(this));
-   }
+   } */
     
     
     function memoryTest(string memory _exampleString) public returns (string memory) {
         stringTest = _exampleString;  // You can modify memory
         string memory newString = stringTest;  // You can use memory within a function's logic
         return newString;  // You can return memory
-        allowance[msg.sender][recipient] = 1;
+        // allowance[msg.sender][recipient] = 1;
     }
 
     function calldataTest(string calldata _exampleString, string calldata _intTest) external returns (string memory, string memory) {
@@ -36,5 +37,11 @@ contract CallandMapTest {
     
     function internalTest(string calldata _internalCall) internal {
         callTest = _internalCall;
+    }
+    
+    //check gas remaining
+    function checkGasAndSig() public view returns(uint256, bytes4){
+        uint256 gasRemaining = gasleft();
+        return(gasRemaining, msg.sig);
     }
 }
