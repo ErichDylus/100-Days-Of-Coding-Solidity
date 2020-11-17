eb7532c7c87863d8d1706849eafb7cbe5e076f07
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2020-11-17 11:51:34 -0500

    Update 2019Day1Part1and2.sol

--- AdventOfCode/2019Day1Part1and2.sol
+++ AdventOfCode/2019Day1Part1and2.sol
@@ -1,4 +1,3 @@
-  
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.7.0;
@@ -6,10 +5,12 @@ pragma solidity 0.7.0;
 contract DayOne2019 {
     
     uint256 fuelTotal;
+    uint256 fuelIndexTotal;
     uint256 totalMass;
     uint256 fuelMassTotal;
     uint256[] moduleMasses = [123265, 68442, 94896, 94670, 145483, 93807, 88703, 139755, 53652, 52754, 128052, 81533, 56602, 96476, 87674, 102510, 95735, 69174, 136331, 51266, 148009, 72417, 52577, 86813, 60803, 149232, 115843, 138175, 94723, 85623, 97925, 141772, 63662, 107293, 130779, 147027, 88003, 77238, 53184, 149255, 71921, 139799, 84851, 104899, 92290, 74438, 55631, 58655, 140496, 110176, 138718, 104768, 93177, 53212, 129572, 69877, 139944, 116062, 51362, 135245, 59682, 128705, 98105, 69172, 89244, 109048, 88690, 62124, 53981, 71885, 59216, 107718, 146343, 138788, 73588, 51648, 122227, 54507, 59283, 101230, 93080, 123120, 148248, 102909, 91199, 105704, 113956, 120368, 75020, 103734, 81791, 87323, 77278, 123013, 58901, 136351, 121295, 132994, 84039, 76813];
     uint256[] massIndex;
+    uint256[] fuelIndex;
     
     constructor() {}
     
@@ -21,15 +22,24 @@ contract DayOne2019 {
         }
     }
     
-    function calculateFuelMass() public {
+   /*function calculateFuelMass() public {
         for (uint256 x = 0; x < moduleMasses.length; x++) {
-            uint256 _fuelMass = massIndex[x] + (massIndex[x]/3 - 2);
+            uint256 _fuelMass = massIndex[x];
+            _fuelMass = _fuelMass/3 - 2;
+            fuelIndex.push(_fuelMass);
             fuelMassTotal = fuelMassTotal + _fuelMass;
-            }
+        }
+    }*/
+    
+    function recalculate() public {
+        for (uint256 x = 0; x < moduleMasses.length; x++) {
+            uint256 _indexResult = massIndex[x];
+            while (_indexResult > 0) {_indexResult = _indexResult/3-2; fuelIndexTotal = fuelIndexTotal + _indexResult;}
+        }
     }
     
     function checkFuelTotal() public returns(uint256) {
-        totalMass = fuelTotal + fuelMassTotal;
+        totalMass = fuelTotal + fuelIndexTotal;
         return(totalMass);
     }
 }
