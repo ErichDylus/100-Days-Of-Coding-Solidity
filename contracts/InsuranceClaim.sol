02888fefa1b9da893f87a144c42cab72c0992cbc
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-05-10 11:34:34 -0400

    Update InsuranceClaim.sol

--- contracts/InsuranceClaim.sol
+++ contracts/InsuranceClaim.sol
@@ -1,111 +1,39 @@
 //SPDX-License-Identifier: MIT
-//adapted from Kleros form:: https://github.com/kleros/erc-792/blob/master/contracts/examples/SimpleEscrowWithERC1497.sol
 //for testing purposes only, not audited nor suitable for any other use
-//TODO: adapt for API3 feed insurance model
-//TODO: compare with https://github.com/lexDAO/LexCorpus/blob/master/contracts/escrow/lexlocker/solidity/LexLocker.sol (and adapt for >=0.8)
+//request resolution of API3 insurance claim through LexLocker (https://github.com/lexDAO/LexCorpus/blob/master/contracts/escrow/lexlocker/solidity/LexLocker.sol)
 
-pragma solidity >=0.8;
+pragma solidity >=0.7.5;   
 
-import "https://github.com/kleros/erc-792/blob/master/contracts/IArbitrable.sol";
-import "https://github.com/kleros/erc-792/blob/master/contracts/IArbitrator.sol";
-import "https://github.com/kleros/erc-792/blob/master/contracts/erc-1497/IEvidence.sol";
-
-contract insuranceClaim is IArbitrable, IEvidence {
-    address payable public payer = payable(msg.sender);
-    address payable public payee;
-    uint256 public value;
-    IArbitrator public arbitrator;
-    uint256 public constant reclamationPeriod = 10 minutes;
-    uint256 public constant arbitrationFeeDepositPeriod = 10 minutes;
-    uint256 public createdAt;
-    uint256 public reclaimedAt;
-
-    enum Status {Initial, Reclaimed, Disputed, Resolved}
-    Status public status;
-
-    enum RulingOptions {RefusedToArbitrate, PayerWins, PayeeWins}
-    uint256 constant numberOfRulingOptions = 2;
-    uint256 constant metaevidenceID = 0;
-    uint256 constant evidenceGroupID = 0;
-
-    //initial with payee, arbitrator, and metaevidence; user may elect to enter LexDAO address as _arbitrator
-    //TODO: update to API3 value
-    constructor(address payable _payee, IArbitrator _arbitrator, string memory _metaevidence) payable {
-        require(msg.value > 0, "Submit escrowed amount");
-        value = msg.value;
-        payee = _payee;
-        arbitrator = _arbitrator;
-        createdAt = block.timestamp;
-
-        emit MetaEvidence(metaevidenceID, _metaevidence);
+import "https://github.com/lexDAO/LexCorpus/blob/master/contracts/escrow/lexlocker/solidity/LexLocker.sol";
+    
+contract InsuranceClaim is LexLocker {
+    
+    address token = 0x0b38210ea11411557c13457d4da7dc6ea731b88a; //API3 token address
+    address api3DAOaddress = 0x9B64177757a0BD2A042446C8d910526476B7Bf57; //API3DAO address, to be updated to v2
+    address lexlocker = 0xD476595aa1737F5FdBfE9C8FEa17737679D9f89a;
+    
+    //initiated by claimant (client for purposes of LexLocker), counterparty is API3 DAO, resolver (API3 Insurance KLeros court or simply LexDAO), claim amount, details of claim, swiftResolver false to prevent nuisance claims
+    constructor(address _resolver, uint256 _claimAmount, string calldata _details) public {
+        client = msg.sender;
+        _resolver = resolver; //may hardcode the Kleros court for API3 insurance when live
+        swiftResolver = false;
+        counterparty = api3DAOaddress;
+        sum = _claimAmount;
     }
-
-    function releaseFunds() public {
-        require(status == Status.Initial, "Not in Initial state.");
-        if (msg.sender != payer)
-            require(block.timestamp - createdAt > reclamationPeriod, "Payer still has time to reclaim.");
-        status = Status.Resolved;
-        payee.transfer(value);
-    }
-
-    function reclaimFunds() public payable {
-        require(status == Status.Initial || status == Status.Reclaimed, "Transaction is not in Initial or Reclaimed state.");
-        require(msg.sender == payer, "Only the payer can reclaim the funds.");
-        if (status == Status.Reclaimed) {
-            require(
-                block.timestamp - reclaimedAt > arbitrationFeeDepositPeriod,
-                "Payee still has time to deposit arbitration fee."
-            );
-            payer.transfer(address(this).balance);
-            status = Status.Resolved;
-        } else {
-            require(block.timestamp - createdAt <= reclamationPeriod, "Reclamation period ended.");
-            require(
-                msg.value == arbitrator.arbitrationCost(""),
-                "Can't reclaim funds without depositing arbitration fee."
-            );
-            reclaimedAt = block.timestamp;
-            status = Status.Reclaimed;
-        }
-    }
-
-    function depositArbitrationFeeForPayee() public payable {
-        require(status == Status.Reclaimed, "Transaction is not in Reclaimed state.");
-        uint256 disputeID = arbitrator.createDispute{value: msg.value}(numberOfRulingOptions, "");
-        status = Status.Disputed;
-        emit Dispute(arbitrator, disputeID, metaevidenceID, evidenceGroupID);
-    }
-
-    function rule(uint256 _disputeID, uint256 _ruling) public override {
-        require(msg.sender == address(arbitrator), "Only the arbitrator can execute this.");
-        require(status == Status.Disputed, "There should be a dispute to execute a ruling.");
-        require(_ruling <= numberOfRulingOptions, "Ruling out of bounds!");
-        status = Status.Resolved;
-        //if payer wins, receives balance, otherwise payee receives balance
-        if (_ruling == uint256(RulingOptions.PayerWins)) payer.transfer(address(this).balance);
-        else payee.transfer(address(this).balance);
-        emit Ruling(arbitrator, _disputeID, _ruling);
-    }
-
-    function remainingTimeToReclaim() public view returns (uint256) {
-        if (status != Status.Initial) revert("Transaction is not in Initial state.");
-        return
-            (createdAt + reclamationPeriod - block.timestamp) > reclamationPeriod
-                ? 0
-                : (createdAt + reclamationPeriod - block.timestamp);
-    }
-
-    function remainingTimeToDepositArbitrationFee() public view returns (uint256) {
-        if (status != Status.Reclaimed) revert("Transaction is not in Reclaimed state.");
-        return
-            (reclaimedAt + arbitrationFeeDepositPeriod - block.timestamp) > arbitrationFeeDepositPeriod
-                ? 0
-                : (reclaimedAt + arbitrationFeeDepositPeriod - block.timestamp);
-    }
-
-    function submitEvidence(string memory _evidence) public {
-        require(status != Status.Resolved, "Transaction is not in Resolve state.");
-        require(msg.sender == payer || msg.sender == payee, "Third parties are not allowed to submit evidence.");
-        emit Evidence(arbitrator, evidenceGroupID, msg.sender, _evidence);
+    
+    /* function lexLockerContract() public {
+         lexlocker = LexLocker(0xD476595aa1737F5FdBfE9C8FEa17737679D9f89a);
+     } */
+    
+    function requestResolution() public NonReentrant returns(uint256) {
+    //Lump sump up for resolution by an arbitration address ( = resolver); $API3 deposit locked immediately for judgment.
+    lexLocker.requestLockerResolution(counterparty, resolver, token, sum, details, swiftResolver);
+    
+    //OR 
+    bytes memory payload = abi.encodeWithSignature("LexLocker(address, address, address, uint256, string calldata, bool)", counterparty, resolver, token, sum, details, swiftResolver);
+    (bool success, bytes memory returnData) = address(lexlocker).call(payload);
+    require(success);
+    
+    return(registration);
     }
 }
