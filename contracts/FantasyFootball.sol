9ef58bdf463594361d4e94b183c9df19c65a401a
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-07-09 10:21:13 -0400

    Update FantasyFootball.sol

--- contracts/FantasyFootball.sol
+++ contracts/FantasyFootball.sol
@@ -5,17 +5,17 @@ pragma solidity 0.7.5;
 /*unaudited and not for mainnet use of any kind, provided without any warranty whatsoever
 **IN PROCESS; NEED TO CODE WINNERS' PAYOUTS
 **@dev create a simple smart escrow contract for a fantasy football league, with contributions and payout in ETH
-**intended to be deployed by commissioner, who is an identified party*/
+**intended to be deployed by commissioner, who is an identified party, after draft is concluded
+**single league, single year*/
 
 contract FantasyFootball {
     
-  //escrow struct to contain basic description of underlying deal, purchase price, ultimate recipient of funds
+  //struct with league name/details and per-team deposit amount
   struct LeagueDetails {
       string description;
       uint256 deposit;
   }
-  
-  LeagueDetails[] public leagues;
+
   address escrowAddress = address(this);
   address payable firstPlace;
   address payable secondPlace;
@@ -25,12 +25,14 @@ contract FantasyFootball {
   uint256 effectiveTime;
   uint256 expirationTime;
   bool isOver;
-  bool isExpired;
   string description;
   mapping(address => bool) public teamWhitelist; //for commissioner to whitelist addresses that may enter league
   mapping(address => bool) public teams; //map whether an address is a team in the league
+  mapping(address => uint256) public points; //map total points per team for standings
   
-  event Expired(bool isExpired);
+  event PayFirstPlace();
+  event PaySecondPlace();
+  event PayThirdPlace();
   event SeasonOver(bool isOver);
   
   modifier restricted() { 
@@ -39,55 +41,42 @@ contract FantasyFootball {
   }
   
   //commissioner deploys contract with description, its deposit amount, and seconds until season expiry
-  constructor(string memory _description, uint256 _deposit, uint256 _secsUntilExpiration) payable {
+  constructor(string memory _description, uint256 _deposit, uint256 _secsUntilSeasonOver) payable {
       require(msg.value >= deposit, "Submit deposit amount");
       commissioner = payable(address(msg.sender));
       deposit = _deposit;
       description = _description;
       teams[commissioner] = true;
       effectiveTime = uint256(block.timestamp);
-      expirationTime = effectiveTime + _secsUntilExpiration;
-      newLeague(description, deposit);
+      expirationTime = effectiveTime + _secsUntilSeasonOver;
+      LeagueDetails(description, deposit);
   }
   
-  //commissioner whitelists addresses who may enter league as a team
+  //commissioner whitelists addresses who may enter league as a team (who participated in the draft)
   function whitelistTeam(address payable _team) public restricted {
       require(!teamWhitelist[_team], "Party already a team");
-      require(!isExpired, "Season expired, too late");
+      require(!isOver, "Season over, too late");
       teamWhitelist[_team] = true;
   }
   
   //team pays deposit and enters league if previously whitelisted by commissioner and not already a team
-  function enterLeague(address payable _team) public payable {
-      require(!teams[msg.sender], "Address is already a team");
-      require(teamWhitelist[msg.sender], "Address has not been whitelisted by commissioner");
-      require(!isExpired, "Season expired, too late");
+  function enterLeague(address payable _teamAddress) public payable {
+      require(!teams[_teamAddress], "Address is already a team");
+      require(teamWhitelist[_teamAddress], "Address has not been whitelisted by commissioner");
+      require(!isOver, "Season over, too late");
       require(msg.value >= deposit, "Submit deposit amount");
-      teams[_team] = true;
+      teams[_teamAddress] = true;
   }
   
-  //create new escrow contract within master structure
-  function newLeague(string memory _description, uint256 _deposit) private {
-      LeagueDetails memory newRequest = LeagueDetails({
-         description: _description,
-         deposit: _deposit
-      });
-      leagues.push(newRequest);
-  }
-  
-  //TODO: weekly increment of points by commissioner to top three addresses
+  //TODO: weekly increment of points by commissioner, publicly verifiable
     
   // check if expired and payout winners if so
   function endSeason() public restricted returns(bool) {
-      if (expirationTime <= uint256(block.timestamp)) {
-            isExpired = true;
-            //buyer.transfer(escrowAddress.balance);
-            emit Expired(isExpired);
-        } else {
-            isOver = true;
-            //seller.transfer(escrowAddress.balance);
-            emit SeasonOver(isOver);
-        }
-        return(isOver);
+      require(expirationTime <= uint256(block.timestamp), "Season still in process.");
+      isOver = true;
+      //firstPlace.transfer(escrowAddress.balance);
+      //emit PayFirstPlace();
+      emit SeasonOver(isOver);
+      return(isOver);
   }
 }
