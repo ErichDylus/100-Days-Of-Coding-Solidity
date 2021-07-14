933576c5943386cd884cde4426b47a7771398679
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-07-14 10:23:50 -0400

    Update AccessByERC20.sol

--- contracts/AccessByERC20.sol
+++ contracts/AccessByERC20.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 // ******** IN PROCESS ********
 
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.6;
 
 //FOR DEMONSTRATION ONLY, not recommended to be used for any purpose, carries absolutely no warranty of any kind
+//currenty testing on Rinkeby
 //@dev create ERC20 holder-gated access to an IPFS link 
 
 interface ERC20 { //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/ERC20.sol
@@ -14,22 +15,22 @@ contract AccessByERC20 {
 
     address token;
     address owner;
-    string IPFSlink;
+    string public IPFSlink;
     ERC20 public erc20;
     mapping(address => uint256) tokenBalance;
     
-    event LinkViewed(address indexed viewer);
+    //event LinkViewed(address indexed viewer);
     
-    constructor(address _token) public { 
+    constructor(address _token, string memory _IPFSlink) public { 
         _token = token;
+        _IPFSlink = IPFSlink;
         erc20 = ERC20(token);
         owner = msg.sender;
     }
     
-    function setIPFSLink(string memory _IPFSlink) external returns(string memory) {
+    function setIPFSLink(string calldata _IPFSlink) external {
         require(msg.sender == owner, "Not authorized to change IPFS link.");
         _IPFSlink = IPFSlink;
-        return(IPFSlink);
     }
     
     //check msg.sender ERC20 balance and assign mapping in order to accessLink()
@@ -38,17 +39,16 @@ contract AccessByERC20 {
         _balance = tokenBalance[msg.sender];
     }
     
-    function checkBalance(address _caller) internal returns(uint256) {
-        return(erc20.balanceOf(msg.sender));
+    function checkBalance(address _caller) internal view returns(uint256) {
+        return(erc20.balanceOf(_caller));
     }
     
-    function accessLink() external view returns(string memory) {
-        if (erc20.balanceOf[msg.sender] > 0) {
+    function accessLink() external returns(string memory) {
+        if (tokenBalance[msg.sender] > 0) {
             return(IPFSlink);
-            emit LinkViewed(msg.sender);
+            //emit LinkViewed(msg.sender);
         } else {
             return("Only tokenholders may access the link.");
         }
     } 
-    
 }
