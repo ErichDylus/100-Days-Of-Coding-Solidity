a049ef0f32a0cd21588360cb1e640cc3954697b0
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2021-09-01 12:12:20 -0400

    Update FantasyFootball.sol

--- contracts/FantasyFootball.sol
+++ contracts/FantasyFootball.sol
@@ -4,7 +4,6 @@ pragma solidity 0.7.5;
 
 /*unaudited and not for mainnet use of any kind, provided without any warranty whatsoever
 **IN PROCESS, NOT OPERATIONAL
-**Needs NON REENTRANT
 **@dev create a simple smart ETH escrow contract for a fantasy football league, 17 week season, with contributions and payout in ETH
 **intended to be deployed by commissioner, who is an identified party, after draft is concluded
 **single league, single year*/
@@ -31,9 +30,9 @@ contract FantasyFootball {
   TeamWeek[] public teamweeks;
   
   event LeagueDetails(string description, uint256 deposit, address commissioner);
-  event PayFirstPlace();
-  event PaySecondPlace();
-  event PayThirdPlace();
+  event PayFirstPlace(address firstPlace);
+  event PaySecondPlace(address secondPlace);
+  event PayThirdPlace(address thirdPlace);
   event SeasonOver(bool isOver);
   
   modifier onlyCommissioner() { 
@@ -41,7 +40,9 @@ contract FantasyFootball {
     _;
   }
   
-  //commissioner deploys contract with description, its deposit amount, and seconds until season expiry
+  //commissioner deploys contract with league description and required deposit amount (along with commissioner's deposit)
+  // @param _description: brief league description/identifier 
+  // @param _deposit: amount of wei required to partipate in league (ETH / 1e18)
   constructor(string memory _description, uint256 _deposit) payable {
       require(msg.value >= deposit, "Submit deposit amount");
       commissioner = payable(address(msg.sender));
@@ -52,6 +53,7 @@ contract FantasyFootball {
   }
   
   //commissioner whitelists addresses who may enter league as a team (who participated in the draft)
+  // @param _team: address of participant in draft, who will subsequently call enterLeague() with deposit 
   function whitelistTeam(address payable _team) public onlyCommissioner returns(bool){
       require(!teamWhitelist[_team], "Party already a team");
       require(!isOver, "Season over, too late");
@@ -71,36 +73,41 @@ contract FantasyFootball {
   }
   
   //weekly increment of points by commissioner, publicly verifiable, one team at a time 
+  // @param _teamAddress: address of team which is confirmed in league and has paid deposit
+  // @param _points: _teamAddress's weekly point total to add to their tally
   function weeklyPoints(address _teamAddress, uint256 _points) public onlyCommissioner {
       require(week < 17, "Season over.");
+      require(!teamweeks.TeamWeek(_teamAddress, week,), "Points already entered for this team this week.");
       // TODO: Check this teamweeks hasn't been pushed yet // require(!TeamWeek(_teamAddress, week, ), "Points already entered for this team this week.");
       points[_teamAddress] += _points;
       teamweeks.push(TeamWeek(_teamAddress, week, true));
   }
   
+  // for commissioner to increment the season week after entering weeklyPoints for each _teamAddress
   function incrementWeek() public onlyCommissioner returns(uint256){
       require(week < 17, "Season over.");
       week++;
       return(week);
   }
 
-  //check point total of a team (TODO: iterate through mapping and return full leaderboard)
+  //check point total of a team 
   function checkPointTotal(address _teamAddress) public view returns(uint256) {
       return(points[_teamAddress]);
   }
     
   //commissioner calls checkPointTotal for each address and passes the top three to this function
   //third place receives their deposit back, second place gets double the deposit amount, first place gets everything else
+  // @param __firstPlace, _secondPlace, _thirdPlace: 1st, 2nd, 3rd place winners verifiable by checkPointTotal
   function endSeason(address payable _firstPlace, address payable _secondPlace, address payable _thirdPlace) public onlyCommissioner returns(bool) {
       require(week > 16, "Season still in process.");
       require(isTeam[_firstPlace] && isTeam[_secondPlace] && isTeam[_thirdPlace], "Re-enter addresses");
       isOver = true;
       _thirdPlace.transfer(deposit);
-      emit PayThirdPlace();
+      emit PayThirdPlace(_thirdPlace);
       _secondPlace.transfer(deposit * 2);
-      emit PaySecondPlace();
+      emit PaySecondPlace(_secondPlace);
       _firstPlace.transfer(address(this).balance);
-      emit PayFirstPlace();
+      emit PayFirstPlace(_firstPlace);
       emit SeasonOver(isOver);
       return(isOver);
   }
